973688231cf0fec1ff1b6b16a62f928a96f59d2f
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-10 17:20:06 +0530

    Update Indus.js

--- components/Indus.js
+++ components/Indus.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Doc from "@/public/doctor.jpg"
 const Indus = () => {
   return (
-    <div class=" max-w-7xl sm:mx-20  px-4 sm:px-6 lg:px-8 py-12">
+    <div class=" max-w-7xl   px-4 sm:px-6 lg:px-8 py-12">
         <div class="text-left mx-10 md:mx-20">
             <h2 class="text-3xl font-bold font-sedan md:text-6xl text-zinc-900 dark:text-white">Industries we serve</h2>
             <p class="mt-4 text-lg md:text-2xl text-zinc-600 dark:text-zinc-300">
@@ -12,8 +12,8 @@ const Indus = () => {
                 optimize operations, and unlock new opportunities for growth.
             </p>
         </div>
-        <div class="relative h-80 mt-10 md:mx-20 justify-center grid grid-cols-2     md:grid-cols-6 ">
-            <div class=" relative bg-black dark:bg-zinc-800  w-20  hover:h-60 md:hover:h-auto md:w-10 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-40  p-6  rounded-lg"
+        <div class="relative h-80 mt-10 mx-5 sm:mx-20 md:mx-32 lg:mx-60 xl:mx-80 xl:last: justify-center grid grid-cols-6  sm:grid-cols-6   md:grid-cols-6 ">
+            <div class=" relative bg-black dark:bg-zinc-800  w-10 sm:w-16 hover:h-80 md:hover:h-auto md:w-20 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-52  p-6  rounded-lg"
             >
             <div className="  absolute z-10  bottom-10  opacity-0 p-6 hover:opacity-100">
                 <h3 class="mt-4  text-sm font-semibold text-white  dark:text-white">Healthcare</h3>
@@ -26,7 +26,7 @@ const Indus = () => {
                 </a>
                 </div>
             </div>
-            <div class="relative bg-black dark:bg-zinc-800 mr-20 w-20 hover:h-60 md:hover:h-auto  md:w-10 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-40 p-6 rounded-lg"
+            <div class="relative bg-black dark:bg-zinc-800 mr-20 w-10 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
             >
                 <div className=" absolute z-10 bottom-10 opacity-0 hover:opacity-100">
 
@@ -40,7 +40,7 @@ const Indus = () => {
                 </a>
             </div>
             </div>
-            <div class="relative bg-black dark:bg-zinc-800 w-20  hover:h-60 md:hover:h-auto  md:w-10 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-40 p-6 rounded-lg"
+            <div class="relative bg-black dark:bg-zinc-800 mr-20 w-10 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
             >
                 <div className=" absolute z-10 bottom-10 opacity-0 hover:opacity-100">
 
@@ -54,7 +54,7 @@ const Indus = () => {
                 </a>
             </div>
             </div>
-            <div class="relative bg-black dark:bg-zinc-800 w-20 hover:h-60 md:hover:h-auto  md:w-10 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-40 p-6 rounded-lg"
+            <div class="relative bg-black dark:bg-zinc-800 mr-20 w-10 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
             >
                 <div className="z-10 absolute bottom-10 opacity-0 hover:opacity-100">
 
@@ -68,7 +68,7 @@ const Indus = () => {
                 </a>
             </div>
             </div>
-            <div class="relative bg-black dark:bg-zinc-800 w-20  hover:h-60 md:hover:h-auto  md:w-10 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-40 p-6 rounded-lg"
+            <div class="relative bg-black dark:bg-zinc-800 mr-20 w-10 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
             >
                 <div className="z-10 absolute bottom-10 opacity-0 hover:opacity-100">
 
@@ -82,7 +82,7 @@ const Indus = () => {
                 </a>
             </div>
             </div>
-            <div class="relative bg-black dark:bg-zinc-800 w-20 hover:h-60 md:hover:h-auto  md:w-10 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-40 p-6 rounded-lg"
+            <div class="relative bg-black dark:bg-zinc-800 mr-20 w-10 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-20 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
             >
                 <div className="z-10 absolute bottom-10 opacity-0 hover:opacity-100">
 
