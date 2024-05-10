4c28e1ea2a8c0707efa3f19d09c496751ff78c10
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-10 17:09:21 +0530

    Update Imgcmp.js

--- components/Imgcmp.js
+++ components/Imgcmp.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Arrow from "@/public/arrow.svg"
 const Imgcmp = () => {
   return (
-  <div class="sm:mx-8 grid m-auto mb-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+  <div class=" grid m-auto mb-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
     
     <div class="hover:hover:bg-zinc-800    bg-black w-auto p-4 rounded-lg space-y-2">
       <div class="text-zinc-400">01</div>
@@ -48,7 +48,9 @@ const Imgcmp = () => {
     </div>
     
     <div class="hover:bg-zinc-800  bg-black p-4 rounded-lg space-y-2">
-      <div class="text-zinc-400">07</div>
+     <div class="text-zinc-400 flex flex-row justify-between"><div className="">07</div>
+<div className=" opacity-0 hover:opacity-100"><Image className='rounded-full' src="icons.svg" alt='icon' width={50} height={50}/> </div>
+</div>
       <h2 class="text-xl text-zinc-200 font-bold">Android</h2>
       <p className='text-zinc-200'>Our team is experienced with various</p>
       <div class="text-blue-300 cursor-pointer"><Image src={Arrow} alt='Arrow' width={30} height={5}/></div>
